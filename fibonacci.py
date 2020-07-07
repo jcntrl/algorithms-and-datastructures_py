c07be8ffa4fd3607e4de5ed6f6cdf6123c29ee26
Justin Cantrall <jcntrl@gmail.com>
2020-07-06 23:33:26 -0600

    cleaned up a bit, more 'pythonic'. no majors.

--- fibonacci.py
+++ fibonacci.py
@@ -23,7 +23,6 @@ def fastfibtopdown(n, memo = {0:0, 1:1}):
         memo[n] = result
         return result
 
-
 def fastfibbottomup(n):
     '''
     bottom up iterative dynamic programming solution - working.
@@ -35,14 +34,16 @@ def fastfibbottomup(n):
     nmin2, nmin1 = 0, 1     #base case
     for fib in range(n-1):  #draw this out on a bottomup table
         result = nmin2 + nmin1  #recurrence relation
-        nmin2 = nmin1
-        nmin1 = result
+        nmin2, nmin1 = nmin1, result
     return result
-
 
 def testfib(typ):
     '''
-    
+    accepts an input to specify which algorithm to run, to what (n)umber to calculate
+    returns the specified fibonacci number and the time required to calculate it.
+    ejects to an early return if number is too large and will cause problems.
+        is there a way to do this with try/except?
+        ... for now this is purely manual
     '''
     n = int(input("input an integer: "))
 
@@ -75,5 +76,3 @@ def testfib(typ):
 
 
 print(testfib(str(input("'fastTD' or 'fastBU' or 'naive': ",))))
-
-# print(fastfibbottomup(8))
