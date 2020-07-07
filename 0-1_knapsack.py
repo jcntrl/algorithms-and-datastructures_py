c0039d8b739747e419861d006133b86ca6e92e08
Justin Cantrall <jcntrl@gmail.com>
2020-07-06 23:19:32 -0600

    added topdown function, but it's TODO

--- 0-1_knapsack.py
+++ 0-1_knapsack.py
@@ -38,16 +38,33 @@ def bottomup(weights, values, limit):
                     )
             else:
                 SVT[item][capacity] = SVT[item-1][capacity]
-    print(SVT)
+    # print(SVT)
     return SVT[-1][-1]
 
 
 
 x = bottomup([3, 3, 2, 5], [6, 7, 8, 9], 5)
 print(x)
-x = bottomup([5,3,4,2], [60,50,70,30], 5)
-print(x)
-x = bottomup([5.1,3.7,4,2.1], [60.1,50.2,70.5,30.8], 6.0)
-print(x)
+# x = bottomup([5,3,4,2], [60,50,70,30], 5)
+# print(x)
+# x = bottomup([5.1,3.7,4,2.1], [60.1,50.2,70.5,30.8], 6.0)
+# print(x)
 
 
+def topdown(weights, values, limit):
+    '''
+    Builds the top-down (recursive) dynamic programming solution table to the 0-1 knapsack problem
+    Objective:  maximize stored value
+    Constraint:     weight capacity
+    Reqs:   import math
+    Inputs: 
+        -list of item weights (num): [3.8, 7, 24.1, ...]
+            -note that these will be converted to ceiling integers
+        -list of item values (num): [0, 1.4, 2.1, 3, ...]
+        -limit: (num)
+            -will be converted to floor integer
+        # TODO feature addition for later, not needed for MVS
+        #   Resolution (num): divisions of weight, 1, 0.1, 0.01, etc. to specify precision of solution
+    Output:
+        returns mixed tuple (value(num), item list[1, 3, ...])
+        '''
