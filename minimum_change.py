195697a2364183a72d501ded1f46b9c7ab0f792c
Justin Cantrall <jcntrl@gmail.com>
2020-07-12 15:56:12 -0600

    changed res to be parameter, not working yet

--- minimum_change.py
+++ minimum_change.py
@@ -1,23 +1,20 @@
-def minchange_TD(AC, TV):
+def minchange_TD(AC, TV, res=0):
     
     if TV == 0: #basecase
-        return 0
-    
-    result = 0
-    
+        return res
+
     for coin in AC:
-        print("coin", coin, "TV", TV)
-        if coin <= TV:
-            print(TV, ' - ', coin, ' = ', TV-coin)
-            TV -= coin
-            result += 1
-            minchange_TD(AC, TV)
-    return result
+            # print("coin", coin, "TV", TV)
+            if coin <= TV:
+                # print(TV, ' - ', coin, ' = ', TV-coin)
+                TV -= coin
+                res += 1
+                print('in loop res', res)
+                minchange_TD(AC, TV, res)
+    print('out of loop res', res)
+    return res
         
 
-# def minchange(AC, TV):
-#     result = 0
-#     result = 
-
-AC=(100, 50, 25, 10, 5, 1)
-print("\nfuncreturn: ", minchange_TD(AC,4))
+AC=(100, 50, 25, 10, 5, 1)  #available coin denominations
+TV = 4  #target value
+print("\noverall function return: ", minchange_TD(AC, TV))
